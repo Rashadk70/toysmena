13b9ffa542fff3a5bebfad5b00efe359f38d0a31
rashadk70 <rashadk70@gmail.com>
2025-02-25 01:14:21 +0400

    Updated client routing setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
